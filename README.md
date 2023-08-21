ce540d1918f0e3f695a01955b60a5ab43e4b5e56
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2023-08-21 14:55:48 -0500

    Readme agregado

---
+++ README.md
@@ -0,0 +1,10 @@
+# Notas:
+Este es mi primer programa en node
+```
+Options:
+      --help Show help                                             [boolean]
+      --version  Show version number                                   [boolean]
+  -b, --base     Es la base de la tabla de multiplicar       [number] [required]
+  -l, --listar   Muestra la tabla de multiplicar      [boolean] [default: false]
+  -h, --hasta    El limite de multiplos a mostrar         [number] [default: 10]
+````
--- app.js
+++ app.js
@@ -12,7 +12,18 @@
  * y listarlo, configurandolo con sus valores y opciones
  * Luego se paso todo lo de yargs a su propio archivo y se importo aca
  * Se creo una bandera h en yargs para poner limite a los multiplos a mostrar
- * 
+ * Se creo un commit de la aplicacion en git
+ * Usando los comandos git init, luego antes de cada commit se ejecuta
+ * git add . , y para hacer el commit git commit -m "El nombre que le quiera
+ * poner al commit"
+ * Para volver a la version hecha en el commit por algun archivo borrado o codigo
+ * se usa git checkout -- . 
+ * A los modulos de node no se les hace un backup porque se pueden recuperar 
+ * con una actualizacion, al igual que archivos que se reemplacen facilmente
+ * Con estos comandos se agrego el proyecto a github
+ * git remote add origin https://github.com/qnderson10/curso_node_tabla.git
+    git branch -M main
+    git push -u origin main
  */
 
 // tabla del 5 en consola y guardarla en un archivo
